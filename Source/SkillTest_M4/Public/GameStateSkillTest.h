c361b0ce836e4033df20e41209fbaa8d9eff7149
Elfamosoronii <122262252+Elfamosoronii@users.noreply.github.com>
2024-09-25 16:35:46 +0200

    coucou

---
+++ Source/SkillTest_M4/Public/GameStateSkillTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "GameStateSkillTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKILLTEST_M4_API AGameStateSkillTest : public AGameState
+{
+	GENERATED_BODY()
+	
+};
